a492b8362b5fe1e5f3b49a3489c80fa1df645097
lucong <332553882@qq.com>
2019-12-30 09:44:47 +0800

    upate

--- config/db.js
+++ config/db.js
@@ -1,7 +1,14 @@
-module.exports = {
+module.exports = process.env.NODE_ENV === 'production' ? {
+  host: '127.0.0.0',
+  port: 3306,
+  dbname: 'db_mall',
+  username: 'root',
+  password: 'sbwd2020',
+  maxLimit: 1000,
+} : {
   host: '59939c0a9a983.gz.cdb.myqcloud.com',
   port: 5579,
-  dbname: '2020_mall_demo',
+  dbname: '2020_huaweiyun_demo',
   username: 'huaweiyun',
   password: 'huaweiyun_2020',
   maxLimit: 1000,
